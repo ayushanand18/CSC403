f91834733b34dccf67ad7bb804b6c4ea5ef5e0dc
Ayush Anand <aa.21u10425@btech.nitdgp.ac.in>
2023-03-18 17:31:54 +0000

    pre-commit fixes

--- algorithms/algorithm_test.py
+++ algorithms/algorithm_test.py
@@ -1,5 +1,17 @@
+from time import time
+from random import randint
 from searching import binary_search
-from sorting import quick_sort_deterministic
+from sorting import quick_sort_deterministic, quick_sort_randomised
 
 assert 3==binary_search([1,2,3,4,5], 4)
 assert [1,2,3,4,5,6] == quick_sort_deterministic([3,2,1,6,5,4])
+assert [1,2,3,4,5,6] == quick_sort_randomised([3,2,1,6,5,4])
+
+# benchmarking performance on a set of 1,000,000 elements
+input_list = [randint(1,1000000) for i in range(1000000)]
+a = time()
+quick_sort_deterministic(input_list)
+b = time()
+quick_sort_randomised(input_list)
+c = time()
+print(f'Deterministic time: {b-a} v/s Randomised: {c-b}')
--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,3 +1,5 @@
+from random import random
+
 def quick_sort_deterministic(arr: list) -> list:
     """
     Deterministic Quicksort algorithm, works in O(nlgn) average time.
@@ -14,6 +16,32 @@ def quick_sort_deterministic(arr: list) -> list:
     lesser_arr, greater_arr = [], []
     # since pivot is the last element, iterate till the second last element
     for i in range(len(arr)-1):
+        if arr[i]<=arr[pivot]:
+            lesser_arr.append(arr[i])
+        else:
+            greater_arr.append(arr[i])
+    # recursively perform quicksort of left and right subarrays and return sorted list
+    return (quick_sort_deterministic(lesser_arr) + [arr[pivot]] +
+            quick_sort_deterministic(greater_arr))
+
+def quick_sort_randomised(arr: list) -> list:
+    """
+    Randomised Quicksort algorithm, works in O(nlgn) average time.
+
+    :param arr: [list] List of elements to sort.
+    :return: [list] Sorted list of elements.
+    """
+    # wel'll pick the last element as the pivot
+    if arr.__len__()<=1:
+        return arr
+    # pick a random index in the array
+    pivot = int(random()*(arr.__len__()-1))
+    # partition around the array
+    lesser_arr, greater_arr = [], []
+    # since pivot is the any element, iterate for all except when equal to pivot
+    for i in range(len(arr)):
+        if i==pivot:
+            continue
         if arr[i]<=arr[pivot]:
             lesser_arr.append(arr[i])
         else:
