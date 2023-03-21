e56e58d0115cfb74af96df80aaa33859f83c40d9
Ayush Anand <aa.21u10425@btech.nitdgp.ac.in>
2023-03-21 12:25:27 +0000

    linting

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,4 +1,12 @@
-from random import random
+"""
+##############################
+#  Ayush Anand               #
+#  ___________               #
+#  21CS8109                  #
+##############################
+"""
+from random import random, randint
+from time import time
 
 def quick_sort_deterministic(arr: list) -> list:
     """
@@ -7,17 +15,21 @@ def quick_sort_deterministic(arr: list) -> list:
     :param arr: [list] List of elements to sort.
     :return: [list] Sorted list of elements.
     """
-    # wel'll pick the last element as the pivot
+    # the base case, if the subarray contains only 1 element
+    # return the same element
     if arr.__len__()<=1:
         return arr
-
+    # pick the last element as the pivot
     pivot = arr.__len__()-1
-    # partition around the array
+    # initialise the left subarray, and the right subarray
     lesser_arr, greater_arr = [], []
     # since pivot is the last element, iterate till the second last element
     for i in range(len(arr)-1):
+        # partition around the pivot element,
+        # all elements lesser equals to pivot into left subarray
         if arr[i]<=arr[pivot]:
             lesser_arr.append(arr[i])
+        # all elements greater than pivot into the right subarray
         else:
             greater_arr.append(arr[i])
     # recursively perform quicksort of left and right subarrays and return sorted list
@@ -31,21 +43,47 @@ def quick_sort_randomised(arr: list) -> list:
     :param arr: [list] List of elements to sort.
     :return: [list] Sorted list of elements.
     """
-    # wel'll pick the last element as the pivot
+    # the base case, if the subarray contains only 1 element
+    # return the same element
     if arr.__len__()<=1:
         return arr
     # pick a random index in the array
     pivot = int(random()*(arr.__len__()-1))
-    # partition around the array
+    # initialise the left subarray, and the right subarray
     lesser_arr, greater_arr = [], []
-    # since pivot is the any element, iterate for all except when equal to pivot
+    # Partitioning the array with respect to the pivot element
+    # since pivot is any element, iterate for all except when equal to pivot
     for i in range(len(arr)):
+        # skip the pivot element from partition
         if i==pivot:
             continue
+        # add the elements lesser equals to the pivot into the left subarray
         if arr[i]<=arr[pivot]:
             lesser_arr.append(arr[i])
+        # add the elements greater equals to the pivot into the right subarray
         else:
             greater_arr.append(arr[i])
     # recursively perform quicksort of left and right subarrays and return sorted list
-    return (quick_sort_deterministic(lesser_arr) + [arr[pivot]] +
-            quick_sort_deterministic(greater_arr))
+    return (quick_sort_randomised(lesser_arr) + [arr[pivot]] +
+            quick_sort_randomised(greater_arr))
+
+print('> Running tests...')
+# benchmarking performance on a set of 1000,000 elements
+input_list = [randint(1,10000) for i in range(1000000)]
+print(f'.. Generated input of length {input_list.__len__()}')
+a = time()
+quick_sort_randomised(input_list)
+b = time()
+print(f'.. Ran randomised in {b-a} second')
+a = time()
+quick_sort_deterministic(input_list)
+b = time()
+print(f'.. Ran deterministic in {b-a} second')
+"""
+> Results
+> -------
+>   Deterministic: 7.305710554122925 sec
+>   Randomised: 6.567975044250488 sec
+> .......
+
+"""
